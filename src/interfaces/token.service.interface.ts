cb6a2aab9d47c9e987a8035189a1ab8b6887dfee
Lucas Mösch <lucasmoesch@live.de>
2025-08-07 17:27:15 +0000

    Updated token interface (again ...)

--- src/interfaces/token.service.interface.ts
+++ src/interfaces/token.service.interface.ts
@@ -1,7 +1,7 @@
 import { InjectionToken, WritableSignal } from "@angular/core";
 
 export interface ITokenService {
-    accessToken: () => WritableSignal<string | null>;
+    accessToken: WritableSignal<string | null>;
 }
 
 export const TOKEN_SERVICE_TOKEN = new InjectionToken<ITokenService>('TokenServiceToken');
